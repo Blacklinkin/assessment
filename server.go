c95ebe6e6ccd53b405a68b0b1833592130bebaf4
kritsadakom <kritsadakom.tk@gmail.com>
2023-01-04 12:57:34 +0700

    initial echo server framework

--- server.go
+++ server.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	//"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
