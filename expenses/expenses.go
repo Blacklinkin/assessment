9a63ad83707184d3b176ced98254df72c5410e2d
kritsadakom <kritsadakom.tk@gmail.com>
2023-01-05 12:18:42 +0700

    add Expenses,Err struct

--- expenses/expenses.go
+++ expenses/expenses.go
@@ -1,1 +1,13 @@
 package expenses
+
+type Expenses struct {
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount string   `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+}
+
+type Err struct {
+	Message string `json:"message"`
+}
